1233d0a97663f6515f1d6344b96008800e4e1384
JCharante <jcharante@gmail.com>
2016-11-11 10:40:30 -0500

    Updated setup to include essential json files

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 setup(
   name = 'Smelter',
-  packages = ['Smelter'], # this must be the same as the name above
-  version = '0.1',
+  packages = ['Smelter', 'required_data'], # this must be the same as the name above
+  version = '0.3',
   description = 'A Python 3.5.2 Library to Censor Strings',
+  package_data={'required_data': ['*.json']},
+  include_package_data=True,
   author = 'JCharante',
   author_email = 'jcharante@gmail.com',
   url = 'https://github.com/AI-Productions/Smelter', # use the URL to the github repo
-  download_url = 'https://github.com/AI-Productions/Smelter/tarball/0.1',
+  download_url = 'https://github.com/AI-Productions/Smelter/tarball/0.3',
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
